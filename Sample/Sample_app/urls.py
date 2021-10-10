f8e56b73559a8acfbc7536c6a5004a127d37b5c7
Vivaan Shiromani <shiromanivivan@gmail.com>
2021-10-11 01:45:32 +0530

    Modified for about and contact pages. Day-1 Last commit !

--- Sample/Sample_app/urls.py
+++ Sample/Sample_app/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from Sample_app import views
 urlpatterns = [
-    path("", views.index, name="Sample_app")
+    path("", views.index, name="Sample_app"),
+    path("about", views.about, name="Sample_app"),
+    path("contact", views.contact, name="Sample_app")
 ]
--- Sample/Sample_app/views.py
+++ Sample/Sample_app/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render ,HttpResponse
 
 def index(request):
     return HttpResponse("Hello World Changed the home page of django !!!")
+def about(request):
+    return HttpResponse("This is about page !!!")
+def contact(request):
+    return HttpResponse("This is a contact page !!!")
 
 # Create your views here.
