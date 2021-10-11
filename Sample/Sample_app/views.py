b580f06d177c8692f8d80b7a3823b19d564b36a9
Vivaan Shiromani <shiromanivivan@gmail.com>
2021-10-11 13:52:50 +0530

    Modified settings, views, important for templates

--- Sample/Sample_app/views.py
+++ Sample/Sample_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render ,HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World Changed the home page of django !!!")
+    # return HttpResponse("Hello World Changed the home page of django !!!")
+    return render(request, "index.htm")
 def about(request):
     return HttpResponse("This is about page !!!")
+
 def contact(request):
     return HttpResponse("This is a contact page !!!")
 
